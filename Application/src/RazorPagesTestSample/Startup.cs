8031eba5efef3d61dafb75141d82f9265aed51d6
David <davejohngoddard@gmail.com>
2024-12-30 22:55:09 +0000

    Challenge6 some change

--- Application/src/RazorPagesTestSample/Startup.cs
+++ Application/src/RazorPagesTestSample/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RazorPagesTestSample.Data;
 
-// Some comment
 namespace RazorPagesTestSample
 {
     public class Startup
